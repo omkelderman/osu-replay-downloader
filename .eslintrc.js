9e25afc7d12669b82ba3ad0d5e0ab11faeaa851a
Olle Kelderman <omkelderman@gmail.com>
2022-10-22 23:23:11 +0200

    add support for writing score-ids higher that max uint32, also added some jsdoc comments and refactored some small things

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     'env': {
         'commonjs': true,
-        'es6': true,
+        'es2020': true,
         'node': true
     },
     'extends': 'eslint:recommended',
@@ -10,7 +10,7 @@ module.exports = {
         'SharedArrayBuffer': 'readonly'
     },
     'parserOptions': {
-        'ecmaVersion': 2018
+        'ecmaVersion': 2020
     },
     'rules': {
         'indent': [
--- WritestreamWrapper.js
+++ WritestreamWrapper.js
@@ -2,51 +2,90 @@ const uleb128 = require('uleb128');
 const crypto = require('crypto');
 
 class WritestreamWrapper {
+    /**
+     * @param {NodeJS.WriteStream} stream the stream to work on
+     * @param {boolean} streamIsClosable if the stream should be closed when done
+     */
     constructor(stream, streamIsClosable) {
         this.stream = stream;
         this.streamIsClosable = streamIsClosable;
     }
 
+    /**
+     * Write arbitrary data to the stream
+     * @param {Uint8Array} data the data to write
+     */
     write(data) {
         this.stream.write(data);
     }
 
+    /**
+     * Write a string to the stream
+     * @param {string} string the string to write
+     */
     writeString(string) {
-        let strBytes = Buffer.from(string, 'utf8');
-        let strHeader = uleb128.encode(strBytes.length);
+        const strBytes = Buffer.from(string, 'utf8');
+        const strHeader = uleb128.encode(strBytes.length);
         strHeader.unshift(0x0B);
 
         this.stream.write(Buffer.from(strHeader));
         this.stream.write(strBytes);
     }
 
+    /**
+     * Write a single byte to the stream
+     * @param {number} byte the single byte to write. It must be >= 0 and <= 255.
+     */
     writeByte(byte) {
-        let buf = Buffer.allocUnsafe(1);
+        const buf = Buffer.allocUnsafe(1);
         buf.writeUInt8(byte);
 
         this.stream.write(buf);
     }
 
+    /**
+     * Write a 16 unsigned number to the stream
+     * @param {number} short a 16 bit unsigned number to write. It must be >= 0 and <= 65535.
+     */
     writeShort(short) {
-        let buf = Buffer.allocUnsafe(2);
+        const buf = Buffer.allocUnsafe(2);
         buf.writeUInt16LE(short);
 
         this.stream.write(buf);
     }
 
+    /**
+     * Write a 32 unsigned number to the stream
+     * @param {number} short a 32 bit unsigned number to write. It must be >= 0 and <= 4294967295.
+     */
     writeInteger(int) {
-        let buf = Buffer.allocUnsafe(4);
+        const buf = Buffer.allocUnsafe(4);
         buf.writeUInt32LE(int);
 
         this.stream.write(buf);
     }
 
+    /**
+     * Write a 64 unsigned number to the stream
+     * @param {number} low a 32 bit unsigned number representing the lower order bits. It must be >= 0 and <= 4294967295.
+     * @param {number} high a 32 bit unsigned number representing the higher order bits. It must be >= 0 and <= 4294967295.
+     */
     writeLong(low, high) {
-        let buf = Buffer.allocUnsafe(8);
+        const buf = Buffer.allocUnsafe(8);
         buf.writeUInt32LE(low, 0);
         buf.writeUInt32LE(high, 4);
 
         this.stream.write(buf);
+    }
+
+    /**
+     * Write a 64 unsigned number to the stream
+     * @param {bigint} bigint a 64 bit unsigned number to write 
+     */
+    writeLongWithBigInt(bigint) {
+        const low = Number(BigInt.asUintN(32, bigint));
+        const high = Number(bigint >> 32n);
+        this.writeLong(low, high);
     }
 
     // WARNING: LOTS OF WERID SHIT THAT LOOKS SUPER DERPY, BUT TRUST ME: IT WORKS!
@@ -54,15 +93,19 @@ class WritestreamWrapper {
     // the tldr is that im writing a js date to a dotnet framework DateTime.Ticks value
     // which is a large number and js doesnt handle large numbers
     // so weird calculations to keep numbers below js max number and calculating a low and high part
+    /**
+     * Write a Date value as the number of "Ticks" in the .net DateTime type
+     * @param {Date} date the Date to write
+     */
     writeDotNetDateTicks(date) {
         const WEIRD_CONSTANT = 429.4967296; // (2^32)/(10^7)
         // get the amount of *seconds* since the start of windows ticks
-        let something = ((date.getTime()/1000)+62135596800);
+        const something = ((date.getTime() / 1000) + 62135596800);
 
         // magic
-        let tmp = something / WEIRD_CONSTANT;
-        let high = Math.floor(tmp);
-        let low = (tmp-high) * WEIRD_CONSTANT * 10000000;
+        const tmp = something / WEIRD_CONSTANT;
+        const high = Math.floor(tmp);
+        const low = (tmp - high) * WEIRD_CONSTANT * 10000000;
 
         this.writeLong(low, high);
     }
@@ -76,7 +119,7 @@ class WritestreamWrapper {
         this.writeInteger(20151228); // game-version. Just a static number like this is fine.
         this.writeString(data.beatmapHash);
         this.writeString(data.score.username);
-        let replayHashData = `${data.score.maxcombo}osu${data.score.username}${data.beatmapHash}${data.score.score}${data.score.rank}`;
+        const replayHashData = `${data.score.maxcombo}osu${data.score.username}${data.beatmapHash}${data.score.score}${data.score.rank}`;
         this.writeString(crypto.createHash('md5').update(replayHashData).digest('hex'));
         this.writeShort(+data.score.count300);
         this.writeShort(+data.score.count100);
@@ -89,20 +132,15 @@ class WritestreamWrapper {
         this.writeByte(+data.score.perfect);
         this.writeInteger(+data.score.enabled_mods);
         this.writeString(''); // graph data, does not have to be available, its not stored on osu servers anyway
-        let replayDate = new Date(data.score.date.replace(' ', 'T') + 'Z');
+        const replayDate = new Date(data.score.date.replace(' ', 'T') + 'Z');
         this.writeDotNetDateTicks(replayDate);
         this.writeInteger(data.replayData.length);
         this.write(data.replayData);
-
-        // finally need to write the replay-id as a 64bit number
-        // yes, this wasnt in that wiki page, but it is needed (thanks osu!lazer :P)
-        // lets cheat and asume it doesnt go above uint32max
-        // if it ever does, no idea what will happen, probably World War III or something
-        this.writeLong(+data.score.score_id, 0);
+        this.writeLongWithBigInt(BigInt(data.score.score_id));
     }
 
     end() {
-        if(this.streamIsClosable) this.stream.end();
+        if (this.streamIsClosable) this.stream.end();
     }
 }
 
